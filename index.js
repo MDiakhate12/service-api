916f12b44a733ec3b04ff8e0dc5ebf9a94b04b74
mdiakhate12 <mdiakhate1297@gmail.com>
2021-01-20 21:16:26 +0000

    Before migrate to GCE

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const connect = require('./config/db');
 const util = require('util')
 // const { exec } = (require('child_process'));
 exec = util.promisify(require('child_process').exec);
+const VmInstance = require('./models/vmInstance');
+
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,41 +14,76 @@ const app = express()
 
 connect();
 
+
 app.use(cors())
 
 app.use(express.json())
 
-const getAvailableRessources = async () => {
+const checkAvailability = async (cpuRequest, memoryRequest, diskRequest) => {
     try {
-        const { stdout: memory } = await exec("free --mega | awk '/Mem/ {print $4/$7*100}'");
+        const { stdout: memoryTotal } = await exec("free --mega | awk '/Mem/ {print $7}'");
+        const { stdout: memoryAvailable } = await exec("free --mega | awk '/Mem/ {print $4}'");
         const { stdout: cpu } = await exec("lscpu | awk '/^CPU\\(s\\):/  {print $2}'");
         const { stdout: diskAvailable } = await exec(`df -hm | awk '/^\\/dev\\// {split($0,a," "); sum += a[4]} END {print sum}'`);
         const { stdout: diskTotal } = await exec(`df -hm | awk '/^\\/dev\\// {split($0,a," "); sum += a[2]} END {print sum}'`);
 
-        let disk = diskAvailable / diskTotal * 100
-        let available = false;
-
-        if (memory >= 80 && cpu > 2 && disk > 50) {
-            available = true
+        if (
+            (memoryAvailable - memoryRequest) * 100 / memoryTotal >= 20 &&
+            cpu - cpuRequest >= 0 &&
+            (diskAvailable - diskRequest) * 100 / diskTotal >= 20
+        ) {
+            return { available: true, memoryAvailable: parseInt(memoryAvailable), diskAvailable: parseInt(diskAvailable), cpuAvailable: parseInt(cpu) }
+        } else {
+            return { available: false, memoryAvailable: parseInt(memoryAvailable), diskAvailable: parseInt(diskAvailable), cpuAvailable: parseInt(cpu) }
         }
-        return { available, cpu: parseFloat(cpu), memory: parseFloat(memory), disk }
 
     } catch (error) {
         console.error(error)
     }
-
-
-
 }
 
-app.get("/available-resources", async (req, res) => {
-    const { numberOfVms, cpu, memory, disk, osImage } = req.body
+app.get("/", async (req, res) => {
+    try {
+        let vmInstances = await VmInstance.find()
+        console.log(vmInstances)
+        res.send(vmInstances);
+    } catch (error) {
+        console.error(error)
+    }
 
-    // GET AVAILABLE RESSOURCES
-    const ressources = await getAvailableRessources()
+})
+
+app.post("/", async (req, res) => {
+    const { name, cpu, memory, disk, osImage, osType, numberOfVm } = req.body
+
+    console.log(`REQUESTED RESOURCES: ${resources} \n`)
+
+
+    // GET AVAILABLE RESOURCES
+    const resources = await checkAvailability(numberOfVm * cpu, numberOfVm * memory, numberOfVm * disk)
+    console.log(`AVAILABLE RESOURCES: ${resources} \n`)
 
     // RETURN TRUE OR FALSE
-    res.send(ressources)
+    if (resources.available) {
+        try {
+            // let os = await OsImage.findOne({ image: osImage, type: osType }).id
+            let newInstance = new VmInstance({
+                name,
+                cpu,
+                memory,
+                disk,
+                osType,
+                osImage
+            })
+            newInstance = await newInstance.save()
+            return res.status(201).send(newInstance)
+        } catch (error) {
+            console.error(error.message)
+            return res.status(500).send("Server error")
+        }
+    } else {
+        return res.send("Insufficient ressources")
+    }
 })
 
 app.listen(PORT, () => {
--- models/vmInstance.js
+++ models/vmInstance.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-const VmInstance = new Schema({
+const VmInstanceSchema = new Schema({
     name: { type: String },
     cpu: { type: Number, default: 1 },
-    ram: { type: Number, default: 2048 },
+    memory: { type: Number, default: 2048 },
     disk: { type: Number, default: 100 },
     os: { type: ObjectId, ref: "osImage" },
+    osType: { type: String, default: "Debian" },
+    osImage: { type: String, default: "Ubuntu 20.04" }
 });
 
-module.exports = mongoose.model('vmInstance', VmInstance);
+const VmInstance = mongoose.model('vmInstances', VmInstanceSchema);
+
+module.exports = VmInstance
