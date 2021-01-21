155d87b68fec582a3a1b63b1ad00cc76a11b5077
MDiakhate12 <46793415+MDiakhate12@users.noreply.github.com>
2021-01-21 10:27:12 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const config = require('config')
-
-const DB_URI = config.get("DB_URI")
+const DB_URI = require('./env')
 
 const connect = async () => {
     try {
