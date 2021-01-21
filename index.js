d7cb46a875707b0180afbb217b7299e8cf481ed9
MDiakhate12 <46793415+MDiakhate12@users.noreply.github.com>
2021-01-21 10:03:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const util = require('util')
 exec = util.promisify(require('child_process').exec);
 const VmInstance = require('./models/vmInstance');
 const ip = require('ip');
+const checkAvailability = require('./utils');
 
 const PORT = process.env.PORT || 8080;
 
@@ -18,29 +19,6 @@ console.log(ip.address())
 app.use(cors())
 
 app.use(express.json())
-
-const checkAvailability = async (cpuRequest, memoryRequest, diskRequest) => {
-    try {
-        const { stdout: memoryTotal } = await exec("free --mega | awk '/Mem/ {print $7}'");
-        const { stdout: memoryAvailable } = await exec("free --mega | awk '/Mem/ {print $4}'");
-        const { stdout: cpu } = await exec("lscpu | awk '/^CPU\\(s\\):/  {print $2}'");
-        const { stdout: diskAvailable } = await exec(`df -hm | awk '/^\\/dev\\// {split($0,a," "); sum += a[4]} END {print sum}'`);
-        const { stdout: diskTotal } = await exec(`df -hm | awk '/^\\/dev\\// {split($0,a," "); sum += a[2]} END {print sum}'`);
-
-        if (
-            (memoryAvailable - memoryRequest) * 100 / memoryTotal >= 20 &&
-            cpu - cpuRequest >= 0 &&
-            (diskAvailable - diskRequest) * 100 / diskTotal >= 20
-        ) {
-            return { available: true, memoryAvailable: parseInt(memoryAvailable), diskAvailable: parseInt(diskAvailable), cpuAvailable: parseInt(cpu) }
-        } else {
-            return { available: false, memoryAvailable: parseInt(memoryAvailable), diskAvailable: parseInt(diskAvailable), cpuAvailable: parseInt(cpu) }
-        }
-
-    } catch (error) {
-        console.error(error)
-    }
-}
 
 app.get("/", async (req, res) => {
     try {
